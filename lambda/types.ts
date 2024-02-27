a6313e61ad88491c7fc36962226fb6fd9c61b3bc
Dan Adajian <dadajian@expediagroup.com>
2024-02-27 07:19:49 -0600

    fix types

--- lambda/types.ts
+++ lambda/types.ts
@@ -1,6 +1,7 @@
-import { PushEvent, PingEvent } from '@octokit/webhooks-types';
 import { components } from '@octokit/openapi-types';
 
 export type EnterprisePayload = { enterprise?: components['schemas']['enterprise'] };
+type PingEvent = components['schemas']['webhook-ping'];
+type PushEvent = components['schemas']['webhook-push'];
 
 export type EnterpriseProxyEvent = (PushEvent | PingEvent) & EnterprisePayload;
